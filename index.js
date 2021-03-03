eb1af6adb5e7c0a87eb55e5c566bc8c46f6419ff
Shailesh Kumar <shailesh@khatabook.com>
2021-03-03 12:17:28 +0530

    added port for heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 class Merchant {
        constructor(name, imageUrl, category, offers) {
